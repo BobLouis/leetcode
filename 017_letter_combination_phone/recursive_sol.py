d8844354084a46967adcfe704e1477c784e58595
BobLouis <bob900316@gmail.com>
2021-07-26 11:17:45 +0800

    017 letter coombination backTrack method added

---
+++ 017_letter_combination_phone/recursive_sol.py
@@ -0,0 +1,26 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"}
+
+        def backTrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backTrack(i+1, curStr+c)
+        if digits:
+            backTrack(0, "")
+        return res
+
+
+s = Solution()
+print(s.letterCombinations("23"))
