0570e3f08a1aa2ea57a8859e78ff60ef0899b11f
BobLouis <bob900316@gmail.com>
2021-08-02 12:11:41 +0800

    first pass 98% 77%

--- 018_4Sum/recursive.py
+++ 018_4Sum/recursive.py
@@ -5,6 +5,36 @@ class Solution:
                 return
             if N == 2:
                 l, r = 0, len(nums)-1
-                while i < r:
+                while l < r:
                     s = nums[l]+nums[r]
-                    if
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        while (l < r and nums[l] == nums[l-1]):
+                            l += 1
+                        while (l < r and nums[r] == nums[r+1]):
+                            r -= 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else:
+                for i in range(0, len(nums) - N + 1):
+                    if (nums[i]*N > target):
+                        break
+                    if(i == 0 or (i > 0 and nums[i] != nums[i-1])):
+                        findNSum(nums[i+1:], target - nums[i],
+                                 N-1, result+[nums[i]], results)
+
+        results = []
+        result = []
+        nums.sort()
+        findNSum(nums, target, 4, result, results)
+        return results
+
+
+s = Solution
+nums = [2, 2, 2, 2, 2]
+target = 8
+print(s.fourSum(nums, nums, target))
