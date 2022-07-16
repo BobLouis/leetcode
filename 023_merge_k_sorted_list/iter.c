a4ce3e3b4118544899ff6ae2bd5eee860dd2a5ea
BobLouis <bob900316@gmail.com>
2022-07-16 16:37:34 +0800

    23 iter C up

---
+++ 023_merge_k_sorted_list/iter.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+ 
+
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    struct ListNode head;
+    struct ListNode *h_ptr = &head;
+
+    if(!list1 && !list2)
+        return NULL;
+
+    while (list1 && list2) {
+        if (list1->val < list2->val) {
+            h_ptr->next = list1;
+            list1 = list1->next;
+            h_ptr = h_ptr->next;
+        } else {
+            h_ptr->next = list2;
+            list2 = list2->next;
+            h_ptr = h_ptr->next;
+        }
+    }
+
+    if(list1)
+        h_ptr->next = list1;
+    if(list2)
+        h_ptr->next = list2;
+
+    return head.next;
+
+}
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize){
+    struct ListNode  *new_head = NULL;
+    
+    for(int i=0;i<listsSize;i++){
+        new_head = mergeTwoLists(new_head,lists[i]);
+    }
+    
+    return new_head;
+}
